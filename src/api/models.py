8cb899d04243a9f0ef7501c45cea6b0c9a40a7a5
pmandries <pandries@aol.com>
2026-02-25 03:09:53 +0000

    models upgrade in progress

--- src/api/models.py
+++ src/api/models.py
@@ -4,31 +4,25 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
-
 #  database for user
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    # first_name: Mapped[str] = mapped_column(String(50)) - we probably won't use
-    # last_name: Mapped[str] = mapped_column(String(50))  - these here
+    # runner_id: Mapped[int] = mapped_column(ForeignKey("runners.id"))
+    username: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-    favorites = relationship("Favorites", back_populates="user")
-    streak = relationship("Streak", back_populates="user")
-    profile = relationship("Runner", back_populates="user")
+    runner_profile = relationship("Runner", back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "username": self.username,
             "email": self.email,
-            # "firstName": self.first_name,
-            # "lastName": self.last_name,
             "is_active": self.is_active,
-            "favorites": self.favorites,
-            "streak": self.streak,
-            "profile": self.profile
+            "runner_profile": self.runner_profile,
             # do not serialize the password, its a security breach
         }
 
@@ -36,53 +30,88 @@ class User(db.Model):
 class Runner(db.Model):
     __tablename__ = "runners"
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_by_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    name = db.Column(db.String(120), nullable=False)    # This may be broken into first_name and last_name
-    phone = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(200), nullable=False)
-    years_running = db.Column(db.Integer, nullable=True)
-    schedule = db.Column(db.String(200), nullable=True)
-    location = db.Column(db.String(200), nullable=True)
-    is_mentor = db.Column(db.Boolean(), nullable=False, default=False)
-    user = relationship("User", back_populates="profile")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    # This may be broken into first_name and last_name
+    # name = db.Column(db.String(120), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    # first_name: Mapped[str] = mapped_column(String(50)) - We may break up first and last name
+    # last_name: Mapped[str] = mapped_column(String(50))
+    # phone = db.Column(db.String(50), nullable=True)
+    phone: Mapped[str] = mapped_column(String(50), nullable=True)
+    # email = db.Column(db.String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    # address = db.Column(db.String(200), nullable=True)
+    address: Mapped[str] = mapped_column(String(200), nullable=True)
+    # years_running = db.Column(db.Integer, nullable=True)
+    years_running: Mapped[int] = mapped_column(nullable=True)    # How do I limit int to 3 digits?
+    # schedule = db.Column(db.String(200), nullable=True)
+    schedule: Mapped[str] = mapped_column(String(200), nullable=True)
+    # location = db.Column(db.String(200), nullable=True)
+    location: Mapped[str] = mapped_column(String(200), nullable=True)
+    # is_mentor = db.Column(db.Boolean(), nullable=False, default=False)
+    is_mentor: Mapped[bool] = mapped_column(nullable=False, default=False)
+    user = relationship("User", back_populates="runner_profile")
+    favorites = relationship(
+        "Favorites", uselist=True,
+        primaryjoin="Runner.id == Favorites.source_runner_id",
+    )
+    streak = relationship(
+        "Streak", back_populates="user",
+        primaryjoin="Streak.streak_by_id == Runner.id",
+    )
 
     def serialize(self):
         return {
             "id": self.id,
-            "user_by_id": self.user_by_id,
+            "user_id": self.user_id,
             "name": self.name,
+            # "firstName": self.first_name, - these are here in case we want to use them
+            # "lastName": self.last_name,
             "phone": self.phone,
             "email": self.email,
             "address": self.address,
             "years_running": self.years_running,
             "schedule": self.schedule,
-            "schedule": self.schedule,
+            "location": self.location,
             "is_mentor": self.is_mentor,
-            "user": self.user
+            "user": self.user,
+            "fav_runners": [fav.serialize() for fav in self.favorites]
         }
 
 
 # database for favorites
+# Runners can favorite other runners and mentors
 class Favorites(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    favorited_by_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user = relationship("User", back_populates="favorites")
+    # this is the runner who is favoriting
+    source_runner_id: Mapped[int] = mapped_column(ForeignKey("runners.id"))
+    source_runner = relationship(
+        "Runner", back_populates="favorites",
+        primaryjoin="Runner.id == Favorites.source_runner_id",
+    )
+    # this is the runner who is being favorited
+    target_runner_id: Mapped[int] = mapped_column(ForeignKey("runners.id"))
+    target_runner = relationship(
+        "Runner",
+        primaryjoin="Runner.id == Favorites.target_runner_id",
+    )
 
     def serialize(self):
         return {
             "id": self.id,
-            "favorited_by_id": self.favorited_by_id,
-            "user": self.user
+            "runner": target_runner.serialize()
         }
 
 
-# database that indicate the streak of the user or how often does the user run
+# database that indicate the streak of the user or how often the user runs
 class Streak(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    streak_by_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user = relationship("User", back_populates="streak")
+    streak_by_id: Mapped[int] = mapped_column(ForeignKey("runners.id"))
+    user = relationship(
+        "Runner", back_populates="streak",
+        primaryjoin="Streak.streak_by_id == Runner.id",
+    )
 
     def serialize(self):
         return {
