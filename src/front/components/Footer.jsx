00e6ce59bf684535a7622ee892958d0ae684653d
OzzieG11 <ozziebgarcia@gmail.com>
2026-03-01 22:22:39 +0000

    Changed the styling of the home page the footer and the favorites button on the navbar as well as moved the runner images from assets to the img folder

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,10 +1,29 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by Deony, Jonathan, Leonard, Ozzie, and Patrick
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3">
+    <div className="container d-flex justify-content-between align-items-center flex-wrap">
+
+      <div>
+        <h4>Running App</h4>
+      </div>
+
+      <div className="text-center">
+        <p className="mb-1">
+          Check the{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://4geeks.com/docs/start/react-flask-template"
+          >
+            template documentation
+          </a>{" "}
+          <i className="fa-solid fa-file"></i> for help.
+        </p>
+        <p className="mb-0">
+          Made with <i className="fa fa-heart text-danger" /> by Deony, Jonathan,
+          Leonard, Ozzie, and Patrick
+        </p>
+      </div>
+
+    </div>
+  </footer>
 );
