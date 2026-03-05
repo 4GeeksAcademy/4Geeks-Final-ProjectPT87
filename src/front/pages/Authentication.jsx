bb2e16cd6c95b16c1ce8d4557b3a5450c454d088
OzzieG11 <ozziebgarcia@gmail.com>
2026-03-05 23:19:04 +0000

    Used prettier to format the documents

--- src/front/pages/Authentication.jsx
+++ src/front/pages/Authentication.jsx
@@ -15,14 +15,11 @@ export const Authentication = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(
-      `${import.meta.env.VITE_BACKEND_URL}/login`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      },
-    );
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
     const data = await response.json();
     if (response.ok) {
       localStorage.setItem("token", data.token);
