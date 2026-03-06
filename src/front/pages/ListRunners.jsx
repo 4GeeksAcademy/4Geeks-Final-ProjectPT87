422f1c48bfed75ccab46024fc6795bb90c677e7b
OzzieG11 <ozziebgarcia@gmail.com>
2026-03-06 01:42:14 +0000

    Styled the create and edit runner profile pages added the as well as formatted everything with prettier

--- src/front/pages/ListRunners.jsx
+++ src/front/pages/ListRunners.jsx
@@ -3,46 +3,50 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import RunnerCard from "../components/RunnerCard.jsx";
+import Page9 from "../assets/img/Page9.jpg";
+import "../styles/listRunners.css";
 
 // This page lists all of the runner cards so that users can scroll through
 export const ListRunners = ({ runner }) => {
-
-  const {store, dispatch, fetchRunner} = useGlobalReducer();
-  const [ runners, setRunners ] = useState([]);
+  const { store, dispatch, fetchRunner } = useGlobalReducer();
+  const [runners, setRunners] = useState([]);
 
   useEffect(() => {
-        fetchRunner()
-        setRunners(store.runners)
-    }, [])
+    fetchRunner();
+    setRunners(store.runners);
+  }, []);
 
-    useEffect(()=>{
-        setRunners(store.runners)
-    }, [store.runners])
-    return (
-        <div className = " contatiner text-center bg-light">
-            <h1 className = "p-3">Runner List</h1>
-            <div>
-                {runners?.length > 0 ? runners.map((runner, index) => {
-                    let pictureNumber = index < 10 ? index : index - 9;
-                    // console.log("pictureNumber: " + pictureNumber);
-                    return (
-                        <RunnerCard
-                            key = {runner.id} 
-                            runner = {runner} 
-                            pictureNumber={pictureNumber}
-                        />
-                    )
-                })
-                :
-                <h2>Add Runner Profile</h2>
-                }
-            </div>
-                <br />
-                <div>
-                    <Link to = "/">
-                        <button className="nav-btn" style = {{marginBottom: 100}}>Return Home</button>
-                    </Link>
-                </div>
-        </div>
-    );
-}; 
+  useEffect(() => {
+    setRunners(store.runners);
+  }, [store.runners]);
+  return (
+    <div className=" contatiner text-center bg-light">
+      <h1 className="p-3">Runner List</h1>
+      <div>
+        {runners?.length > 0 ? (
+          runners.map((runner, index) => {
+            let pictureNumber = index < 10 ? index : index - 9;
+            // console.log("pictureNumber: " + pictureNumber);
+            return (
+              <RunnerCard
+                key={runner.id}
+                runner={runner}
+                pictureNumber={pictureNumber}
+              />
+            );
+          })
+        ) : (
+          <h2>Add Runner Profile</h2>
+        )}
+      </div>
+      <br />
+      <div>
+        <Link to="/">
+          <button className="nav-btn" style={{ marginBottom: 100 }}>
+            Return Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
